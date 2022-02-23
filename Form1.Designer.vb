8b7566a77eadd9dd7cb109165d78e1e8bb699b55
Diego <diegoangu2000@gmail.com>
2022-02-22 20:28:14 -0600

    first

---
+++ Form1.Designer.vb
@@ -0,0 +1,119 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.MasterDataSet = New DemoForm.masterDataSet()
+        Me.DatabaseBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.DatabaseTableAdapter = New DemoForm.masterDataSetTableAdapters.DatabaseTableAdapter()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.XDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.YDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Button2 = New System.Windows.Forms.Button()
+        CType(Me.MasterDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DatabaseBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(71, 231)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'MasterDataSet
+        '
+        Me.MasterDataSet.DataSetName = "masterDataSet"
+        Me.MasterDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'DatabaseBindingSource
+        '
+        Me.DatabaseBindingSource.DataMember = "Database"
+        Me.DatabaseBindingSource.DataSource = Me.MasterDataSet
+        '
+        'DatabaseTableAdapter
+        '
+        Me.DatabaseTableAdapter.ClearBeforeFill = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AutoGenerateColumns = False
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.XDataGridViewTextBoxColumn, Me.YDataGridViewTextBoxColumn})
+        Me.DataGridView1.DataSource = Me.DatabaseBindingSource
+        Me.DataGridView1.Location = New System.Drawing.Point(297, 97)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(252, 225)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'XDataGridViewTextBoxColumn
+        '
+        Me.XDataGridViewTextBoxColumn.DataPropertyName = "X"
+        Me.XDataGridViewTextBoxColumn.HeaderText = "X"
+        Me.XDataGridViewTextBoxColumn.Name = "XDataGridViewTextBoxColumn"
+        '
+        'YDataGridViewTextBoxColumn
+        '
+        Me.YDataGridViewTextBoxColumn.DataPropertyName = "Y"
+        Me.YDataGridViewTextBoxColumn.HeaderText = "Y"
+        Me.YDataGridViewTextBoxColumn.Name = "YDataGridViewTextBoxColumn"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(71, 299)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.MasterDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DatabaseBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents MasterDataSet As masterDataSet
+    Friend WithEvents DatabaseBindingSource As BindingSource
+    Friend WithEvents DatabaseTableAdapter As masterDataSetTableAdapters.DatabaseTableAdapter
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents XDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents YDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents Button2 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,51 @@
+﻿Imports System.Data.SqlClient
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'MasterDataSet.Database' table. You can move, or remove it, as needed.
+        Me.DatabaseTableAdapter.Fill(Me.MasterDataSet.Database)
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim connetionString As String
+        Dim cnn As SqlConnection
+        connetionString = "Data Source=localhost;Initial Catalog=master;Integrated Security=True"
+        cnn = New SqlConnection(connetionString)
+        cnn.Open()
+        Dim Command As SqlCommand
+        Dim datareader As SqlDataReader
+        Dim Sql, Output As String
+        Output = ""
+
+        Sql = "Select * from [master].[dbo].[Database]"
+        Command = New SqlCommand(Sql, cnn)
+        datareader = Command.ExecuteReader()
+
+        While datareader.Read()
+
+            Output = Output + datareader.GetValue(0) + "-" + datareader.GetValue(1) + "\n"
+        End While
+
+        MessageBox.Show(Output)
+        datareader.Close()
+        Command.Dispose()
+
+        cnn.Close()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        'Dim intData(30)   ' an array of 31 elements
+        Dim strData(20) As String   ' an array of 21 strings
+        Dim twoDarray(10, 20) As Integer    'a two dimensional array of integers
+        Dim ranges(10, 100)  'a two dimensional array
+
+        Dim intData() As Integer = {12, 16, 20, 24, 28, 32}
+
+
+        Dim names() As String = {"Karthik", "Sandhya",
+        "Shivangi", "Ashwitha", "Somnath"}
+        Dim miscData() As Object = {"Hello World", 12D, 16UI, "A"c}
+
+    End Sub
+End Class
